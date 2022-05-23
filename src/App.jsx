4198d78cde0f5ae007be7ebfe74a078992eb003c
miriam isela flores antonio <isc18350307@gmail.com>
2022-05-23 09:56:07 -0500

    calculadora lista

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+import Calculadora from '/home/sc3/Escritorio/18350303/calculadora/src/componentes/calculadora/Calculadora'
+
+function App(){
+  return(
+    <main className = 'container'>
+      <Calculadora />
+    </main>
+  )
+}
+
+
+
+export default App
